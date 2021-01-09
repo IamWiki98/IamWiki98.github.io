2846ec02d4aa610353e81fb5e7f80922477d572b
IamWiki98 <56855644+IamWiki98@users.noreply.github.com>
2021-01-09 18:36:54 +0200

    Anchor history and offset fixed
    [Change]
    1. History - not affected by anchors since they are replaced by the AnchorScrollToView.js
    2. Anchor location - since the website has a navbar which has its own height, the scrollIntoView function won't scroll correctly since it doesn't take the navbar height into consideration. An offset is added with window.ScrollTo function.

---
+++ scripts/AnchorScrollToView.js
@@ -0,0 +1,23 @@
+
+// Original JavaScript code by Chirp Internet: www.chirp.com.au
+// Please acknowledge use of this code by including this header.
+
+window.addEventListener("DOMContentLoaded", function(e) {
+  var links = document.getElementsByTagName("A");
+  for(var i=0; i < links.length; i++) {
+    if(!links[i].hash) continue;
+    if(links[i].origin + links[i].pathname != self.location.href) continue;
+    (function(anchorPoint) {
+		
+      links[i].addEventListener("click", function(e) {
+		const offset = document.getElementsByClassName("navbar")[0].offsetHeight;
+		const offsetTop = anchorPoint.offsetTop - offset;
+        window.scrollTo({
+			top: offsetTop,
+			behavior: "smooth"
+		});
+        e.preventDefault();
+      }, false);
+    })(document.getElementById(links[i].hash.replace(/#/, "")));
+  }
+}, false);
