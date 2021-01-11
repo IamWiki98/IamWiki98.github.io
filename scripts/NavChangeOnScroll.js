2ab81b3e4ea410efb1239346fc6195a6050180df
IamWiki98 <56855644+IamWiki98@users.noreply.github.com>
2021-01-11 16:50:57 +0200

    Navigation bar on scroll
    [Change]
    1. Active Nav Anchor - the current shown on screen section will be marked as active in the navigation bar.

--- scripts/NavChangeOnScroll.js
+++ scripts/NavChangeOnScroll.js
@@ -1,6 +1,7 @@
 const navbar = document.getElementsByClassName("navbar")[0];
 const bannerSection = document.querySelector(".banner-area");
 
+const offsetMargin = 200;
 const bannerSectionOptions = {
 	rootMargin: "-200px 0px 0px 0px"
 };
@@ -17,3 +18,29 @@ const bannerSectionObserver = new IntersectionObserver(function(entries, bannerS
 bannerSectionOptions);
 
 bannerSectionObserver.observe(bannerSection);
+
+
+window.addEventListener('scroll', event => {
+	let navigationLinks = document.querySelectorAll('.navbar .nav a');
+	let fromTop = window.scrollY;
+	
+	navigationLinks.forEach(link => {
+		let section = document.querySelector(link.hash);
+		
+		if(section.offsetTop - offsetMargin <= fromTop &&
+			section.offsetTop + section.offsetHeight - offsetMargin > fromTop
+			&& (window.innerHeight + window.scrollY) < document.body.offsetHeight)
+			{
+				link.classList.add('nav-active');
+			} 
+			else
+			{
+				link.classList.remove('nav-active');
+			}
+	});
+	
+	if((window.innerHeight + window.scrollY) >= document.body.offsetHeight)
+	{
+		navigationLinks[navigationLinks.length - 1].classList.add('nav-active');
+	}
+});
